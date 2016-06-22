8699eb5ff5ffff8e1cdfb42911429f14e9c5f5b0
Juan David Monoya <juanmonto5@gmail.com>
2016-06-22 14:50:07 -0500

    archivos de Jquery

---
+++ 10JqueryUnsolved/01/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$('p').css('border','4px solid red');//cambia secciones
+$('.lead').css('border','4px solid blue'); //cambia clases
+$('#lesson-1').css('border','4px solid #000000'); //cambia ids
+$('*').css('border','1px solid #000000'); //cambia todo el documento
+
+});
+
+
+$(function(){
+
+
+
+});
+
+
+$(function(){
+
+
+});
